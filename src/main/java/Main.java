d36552e95cf7cae522ca3619d2c7f9d1c6573c79
Lloyd Perez <lloyd4965jr@yahoo.com>
2022-09-16 13:57:51 -0400

    Fixed Mistake

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,19 +8,15 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello! Welcome to an only addition calculator.\nBelow when you enter two numbers, you should be able to get the sum of both numbers.\nIf you enter in anything that is not a number, you will exit the calculator.");
+        int sum = 0;
+        System.out.println("---Sum of Input---");
+        System.out.println("Enter number: ");
+        Scanner input = new Scanner(System.in);
+        int add = input.nextInt();
 
-        int sum;
-
-        while (true) {
-            Scanner scan = new Scanner(System.in);
-            System.out.print("Enter the first number: ");
-            int add = scan.nextInt();
-
-            System.out.print("Enter the second number: ");
-            int numb = scan.nextInt();
-            sum = add + numb;
-            System.out.println("The sum is: " + sum + " Enter any letter key to exit.");
+        for( int i = 1; i <= add; i++) {
+            sum += i;
         }
+        System.out.println(sum);
     }
 }
